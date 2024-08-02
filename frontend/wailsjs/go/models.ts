c66980cff6d519e7ae8a455243d43e8e1a610f94
Two-Miao <2545409374@qq.com>
2024-08-02 14:10:41 +0800

    Add files via upload

---
+++ frontend/wailsjs/go/models.ts
@@ -0,0 +1,227 @@
+export namespace gtools {
+	
+	export class updatParam {
+	    type: string;
+	    value: {[key: string]: string};
+	
+	    static createFrom(source: any = {}) {
+	        return new updatParam(source);
+	    }
+	
+	    constructor(source: any = {}) {
+	        if ('string' === typeof source) source = JSON.parse(source);
+	        this.type = source["type"];
+	        this.value = source["value"];
+	    }
+	}
+
+}
+
+export namespace internal {
+	
+	export class CmdItem {
+	    id: number;
+	    name: string;
+	    type: string;
+	    state: number;
+	    port: string;
+	    start: string;
+	    stop: string;
+	    // Go type: time
+	    created: any;
+	
+	    static createFrom(source: any = {}) {
+	        return new CmdItem(source);
+	    }
+	
+	    constructor(source: any = {}) {
+	        if ('string' === typeof source) source = JSON.parse(source);
+	        this.id = source["id"];
+	        this.name = source["name"];
+	        this.type = source["type"];
+	        this.state = source["state"];
+	        this.port = source["port"];
+	        this.start = source["start"];
+	        this.stop = source["stop"];
+	        this.created = this.convertValues(source["created"], null);
+	    }
+	
+		convertValues(a: any, classs: any, asMap: boolean = false): any {
+		    if (!a) {
+		        return a;
+		    }
+		    if (a.slice) {
+		        return (a as any[]).map(elem => this.convertValues(elem, classs));
+		    } else if ("object" === typeof a) {
+		        if (asMap) {
+		            for (const key of Object.keys(a)) {
+		                a[key] = new classs(a[key]);
+		            }
+		            return a;
+		        }
+		        return new classs(a);
+		    }
+		    return a;
+		}
+	}
+	export class ConfigItem {
+	    id: number;
+	    name: string;
+	    value: string;
+	    type: string;
+	    state: number;
+	    // Go type: time
+	    created: any;
+	
+	    static createFrom(source: any = {}) {
+	        return new ConfigItem(source);
+	    }
+	
+	    constructor(source: any = {}) {
+	        if ('string' === typeof source) source = JSON.parse(source);
+	        this.id = source["id"];
+	        this.name = source["name"];
+	        this.value = source["value"];
+	        this.type = source["type"];
+	        this.state = source["state"];
+	        this.created = this.convertValues(source["created"], null);
+	    }
+	
+		convertValues(a: any, classs: any, asMap: boolean = false): any {
+		    if (!a) {
+		        return a;
+		    }
+		    if (a.slice) {
+		        return (a as any[]).map(elem => this.convertValues(elem, classs));
+		    } else if ("object" === typeof a) {
+		        if (asMap) {
+		            for (const key of Object.keys(a)) {
+		                a[key] = new classs(a[key]);
+		            }
+		            return a;
+		        }
+		        return new classs(a);
+		    }
+		    return a;
+		}
+	}
+	export class MdPath {
+	    id: number;
+	    path: string;
+	    type: number;
+	    fname: string;
+	    // Go type: time
+	    created: any;
+	
+	    static createFrom(source: any = {}) {
+	        return new MdPath(source);
+	    }
+	
+	    constructor(source: any = {}) {
+	        if ('string' === typeof source) source = JSON.parse(source);
+	        this.id = source["id"];
+	        this.path = source["path"];
+	        this.type = source["type"];
+	        this.fname = source["fname"];
+	        this.created = this.convertValues(source["created"], null);
+	    }
+	
+		convertValues(a: any, classs: any, asMap: boolean = false): any {
+		    if (!a) {
+		        return a;
+		    }
+		    if (a.slice) {
+		        return (a as any[]).map(elem => this.convertValues(elem, classs));
+		    } else if ("object" === typeof a) {
+		        if (asMap) {
+		            for (const key of Object.keys(a)) {
+		                a[key] = new classs(a[key]);
+		            }
+		            return a;
+		        }
+		        return new classs(a);
+		    }
+		    return a;
+		}
+	}
+	export class TodoItem {
+	    id: number;
+	    groupId: number;
+	    title: string;
+	    content: string;
+	    tags: string;
+	    // Go type: time
+	    date: any;
+	    hasContent: boolean;
+	    done: boolean;
+	    level: number;
+	    start: number;
+	    end: number;
+	    // Go type: time
+	    expired: any;
+	    // Go type: time
+	    updated: any;
+	
+	    static createFrom(source: any = {}) {
+	        return new TodoItem(source);
+	    }
+	
+	    constructor(source: any = {}) {
+	        if ('string' === typeof source) source = JSON.parse(source);
+	        this.id = source["id"];
+	        this.groupId = source["groupId"];
+	        this.title = source["title"];
+	        this.content = source["content"];
+	        this.tags = source["tags"];
+	        this.date = this.convertValues(source["date"], null);
+	        this.hasContent = source["hasContent"];
+	        this.done = source["done"];
+	        this.level = source["level"];
+	        this.start = source["start"];
+	        this.end = source["end"];
+	        this.expired = this.convertValues(source["expired"], null);
+	        this.updated = this.convertValues(source["updated"], null);
+	    }
+	
+		convertValues(a: any, classs: any, asMap: boolean = false): any {
+		    if (!a) {
+		        return a;
+		    }
+		    if (a.slice) {
+		        return (a as any[]).map(elem => this.convertValues(elem, classs));
+		    } else if ("object" === typeof a) {
+		        if (asMap) {
+		            for (const key of Object.keys(a)) {
+		                a[key] = new classs(a[key]);
+		            }
+		            return a;
+		        }
+		        return new classs(a);
+		    }
+		    return a;
+		}
+	}
+
+}
+
+export namespace util {
+	
+	export class Resp {
+	    code: number;
+	    msg: string;
+	    data: any;
+	
+	    static createFrom(source: any = {}) {
+	        return new Resp(source);
+	    }
+	
+	    constructor(source: any = {}) {
+	        if ('string' === typeof source) source = JSON.parse(source);
+	        this.code = source["code"];
+	        this.msg = source["msg"];
+	        this.data = source["data"];
+	    }
+	}
+
+}
+
